f0292731635b7d848321c86ed1f0b02371dff3c2
Sohel440 <codewithsohel2003@gmail.com>
2026-02-19 14:57:08 +0530

    feat: update all the features

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,36 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import errorHandler from './src/middlewares/error.middleware.js';
+import connectDB from './src/config/db.js';
+import userRouter from './src/routes/user.routes.js';
+import cookieParser from 'cookie-parser';
+import doctorRouter from './src/routes/doctor.routes.js';
+import patientRouter from './src/routes/patient.routes.js';
+import adminRouter from './src/routes/admin.routes.js';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser())
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    credentials: true,
+  })
+);
 
 
 const PORT = process.env.PORT|| 3000;
 
+
+connectDB();
+
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/doctor', doctorRouter);
+app.use('/api/v1/patient', patientRouter);
+app.use('/api/v1/admin', adminRouter);
 
 
 app.use(errorHandler); // add global middleware for error handler
