7f0101fca760c93ffc6c9a574bd32f7a44a86e78
Sohel440 <codewithsohel2003@gmail.com>
2026-02-19 15:53:27 +0530

    feat: add all routes and Toas container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,75 @@
-import React from 'react'
-import {BrowserRouter as Router , Route , Routes} from 'react-router-dom'
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import { useDispatch } from "react-redux";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import { Toaster } from "react-hot-toast";
+import DoctorDashboard from "./pages/doctor/DoctorDashboard";
+import DoctorAvailability from "./pages/patient/DoctorAvailability";
+
+import PatientDashboard from "./pages/patient/PatientDashboard";
+import DoctorList from "./pages/patient/DoctorList";
+import MyAppointments from "./pages/patient/Myappointments";
+import Profile from "./pages/auth/Profile";
+import { loadUser } from "./features/auth/authThunk";
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadUser());
+  }, [dispatch]);
   return (
     <div>
-       app
-    </div>
-  )
-}
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
-export default App
+          <Route
+            path="/patient"
+            element={
+              <ProtectedRoute role="patient">
+                <PatientDashboard />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<DoctorList />} />
+            <Route path="appointments" element={<MyAppointments />} />
+            <Route path=":id" element={<DoctorAvailability />} />
+          </Route>
+
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute role="admin">
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/doctor"
+            element={
+              <ProtectedRoute role="doctor">
+                <DoctorDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute role="doctor">
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
